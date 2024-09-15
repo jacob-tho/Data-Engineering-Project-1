5d077e6bf8cd9e49ac3143193fb9cc979683f0ff
Jacob <thorwarth.jacob@gmail.com>
2024-09-15 16:40:27 +0200

    Grundlegende Visualisierung von Zeitreihen implementiert

--- mining.py
+++ mining.py
@@ -1,3 +1,9 @@
+import mysql.connector
+import os
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates
+
 """
 Normale Prozedur: EDA; PDA
 Time Series visulalisieren. Lineare Regression?. Allgemein schauen, was man mit Time-Series so machen kann.
@@ -5,3 +11,25 @@ Time Series als ODE auffassen? Typische TS verwenden, Stückweise zu Neural ODE 
 Vergleiche ziehen zu "klassischen" ML Algorithmen, wie schneiden diese jeweils ab?
 Umformung in nicht - time series -> Feature Engineering
 """
+mydb = mysql.connector.connect(
+host="localhost",
+user="root",
+password= os.getenv("MYSQL_PASSWORD"),
+database="finance"
+)
+mycursor = mydb.cursor()
+
+mycursor.execute(f"SELECT timestamp, close FROM dell")
+rows = mycursor.fetchall()
+x_axis = []
+y_axis = []
+for row in rows:
+    date_part = row[0]
+    close = row[1]
+    cleaned = (datetime.datetime(date_part.year, date_part.month, date_part.day), close)
+    x_axis.append(cleaned[0])
+    y_axis.append(cleaned[1])
+
+plt.plot(x_axis, y_axis)
+plt.show()
+#plt.show()
